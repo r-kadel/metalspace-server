4cdd4c60f89f5c548572f8c049863563f720d18a
Ryan Kadel <ryan.kadel88@gmail.com>
2020-05-03 22:13:19 -0400

    test

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'https://metalspace.now.sh/',
     API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
+    process.env.REACT_APP_API_BASE_URL || 'https://metalspace.now.sh/',
     DATABASE_URL:
     process.env.DATABASE_URL || 'postgresql://postgres@localhost/metalspace',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
